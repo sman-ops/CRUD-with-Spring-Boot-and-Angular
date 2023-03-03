b38ced2d6f68ac4c15b6a9f3411c75f9ce0c2f97
sman-ops <slimen.ghenimi@gmail.com>
2023-03-03 20:40:42 +0100

    Setup two parts of project

---
+++ ApiSpringBoot/src/main/java/com/example/crud/ApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringBootApplication.class, args);
+	}
+
+}
