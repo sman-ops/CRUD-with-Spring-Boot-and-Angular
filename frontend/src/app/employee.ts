fb05063abd01467bf511fa5617bb14f06f590758
sman-ops <slimen.ghenimi@gmail.com>
2023-03-04 20:08:38 +0100

    add all the methods in service in Angular

---
+++ frontend/src/app/employee.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+  id: number;
+  name: string;
+  email: string;
+  jobTitle: string;
+  phone: string;
+  imgUrl: string;
+  employeeCode: String;
+}
+// this represent  the type of data  that's going to be returning  whenever  we mak those calls from the backend
+// the employee looks like from the backend
