9bc358b1b75da8eac088cf7792f39571a934f530
sman-ops <slimen.ghenimi@gmail.com>
2023-03-05 16:39:33 +0100

    get list of employees and create model of add employee

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -20,6 +20,7 @@ export class AppComponent implements OnInit {
   public getEmployees(): void {
     this.employeeService.getEmployees().subscribe(
       (response: Employee[]) => {
+        console.log(response);
         this.employees = response;
       },
       (error) => {
--- frontend/src/app/employee.ts
+++ frontend/src/app/employee.ts
@@ -5,7 +5,7 @@ export interface Employee {
   jobTitle: string;
   phone: string;
   imgUrl: string;
-  employeeCode: String;
+  employeeCode: string;
 }
 // this represent  the type of data  that's going to be returning  whenever  we mak those calls from the backend
 // the employee looks like from the backend
